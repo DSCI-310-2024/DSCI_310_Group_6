e8e2cf050d307cd045c9f0df2cdd4f3433a96737
“Dia <diazavery6@gmail.com>
2024-04-02 16:56:03 -0700

    Move distribution_plot.R to R directory

--- tests/testthat/test-distribution_plot.R
+++ tests/testthat/test-distribution_plot.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(palmerpenguins)
+library(ggplot2)
+library(vdiffr)
+source("../../R/distribution_plot.R")
+
+penguins_dist <- distribution_plot(penguins, bill_length_mm) +
+  labs(x="Bill length (mm)", y="Count")
+penguins_dist
+
+test_that("refactoring our code should not change our plot", {
+  expect_doppelganger("distribution plot", penguins_dist)
 })
